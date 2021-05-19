394ff3b8953492cf245ba0762645e4682e644b06
essecitretretre <77722895+essecitretretre@users.noreply.github.com>
2021-05-19 09:45:39 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Casadei.Samuele._4H.SaveRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SaveRecord - 2021 - samuele.casadei@ittsrimini.edu.it");
+
+            //Leggere un file csv con i comuni s trasformarlo in una list<comune>
+            Comuni c = new Comuni("Comuni.csv");
+            Console.WriteLine($"Ho letto{c.count}");
+            
+            //Scrivere la List<Comune> in un file binario
+            //Rileggere il file binario in una List<Comune>
+        }
+    }
+}
